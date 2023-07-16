c2289ec94c40c73b15963bac5037ad81a6ad835d
Bonjanette <bonjanette@yahoo.com>
2023-07-15 21:51:12 -0400

    working bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -5,23 +5,55 @@ let datasets;
 d3.json(url).then(function (data) {
   BBdata = data;
   datasets = data.names;
-  console.log(BBdata);
-  console.log(datasets);
   generateOptions();
 });
-  function generateOptions() {
-    let dropDown = document.getElementById("selDataset");
-    for (let i = 0; i < datasets.length; i++) {
-      let dataset = document.createElement("option");
-      dataset.text = datasets[i];
-      dataset.value = parseInt(datasets[i]);
-      dropDown.appendChild(dataset);
-    }
-  }
 
-  function optionChanged(selectedDataset) {
-    // let dropDown = document.getElementById("selDataset");
-    // let selectedDataset = dropDown.value;
-    console.log(selectedDataset);
+function generateOptions() {
+  let dropDown = document.getElementById("selDataset");
+  for (let i = 0; i < datasets.length; i++) {
+    let dataset = document.createElement("option");
+    dataset.text = datasets[i];
+    dataset.value = parseInt(datasets[i]);
+    dropDown.appendChild(dataset);
   }
+}
 
+function optionChanged(selectedDataset) {
+  createBarChart(selectedDataset);
+}
+
+function createBarChart(selectedDataset) {
+  let samples = BBdata.samples;
+  let chosenSample = samples.find(function (sample) {
+    return sample.id === selectedDataset;
+  });
+
+  let samplevals = chosenSample.sample_values;
+  let otuVals = chosenSample.otu_ids;
+  let otuLabels = chosenSample.otu_labels;
+
+  let barVals = samplevals.slice(0, 10);
+  let labels = otuVals.slice(0, 10);
+  let ylabels = [];
+  
+  for (let i = 0; i < 10; i++) {
+    ylabels.push(`OTU ${labels[i]}`);
+  }
+  let hoverText = otuLabels.slice(0, 10);
+
+  let barTrace = {
+    x: barVals,
+    y: ylabels,
+    text: hoverText,
+    name: "Bar",
+    type: "bar",
+    orientation: "h"
+  }
+  let traceData = [barTrace];
+
+  let layout = {
+    title: "Operational Taxonomic Units<br> AKA Microbes Found"
+  }
+  Plotly.newPlot("bar", traceData, layout);
+  console.log(barVals, labels, hoverText);
+}
