306e48858080c007c1a206781d5ba57426034b32
Bonjanette <bonjanette@yahoo.com>
2023-07-15 20:11:10 -0400

    working dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,27 @@
 const url =
   "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+let datasets;
 
 d3.json(url).then(function (data) {
-  console.log(data);
+  BBdata = data;
+  datasets = data.names;
+  console.log(BBdata);
+  console.log(datasets);
+  generateOptions();
 });
+  function generateOptions() {
+    let dropDown = document.getElementById("selDataset");
+    for (let i = 0; i < datasets.length; i++) {
+      let dataset = document.createElement("option");
+      dataset.text = datasets[i];
+      dataset.value = parseInt(datasets[i]);
+      dropDown.appendChild(dataset);
+    }
+  }
+
+  function optionChanged(selectedDataset) {
+    // let dropDown = document.getElementById("selDataset");
+    // let selectedDataset = dropDown.value;
+    console.log(selectedDataset);
+  }
+
